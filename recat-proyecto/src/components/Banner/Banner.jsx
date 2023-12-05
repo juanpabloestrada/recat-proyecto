8ee51ed95263f5b7afa7d7975b20bb67e5f06eb0
juanpabloestrada <pablo.estrada.1983.18@gmail.com>
2023-12-04 18:45:42 -0600

    inio proyecto

---
+++ recat-proyecto/src/components/Banner/Banner.jsx
@@ -0,0 +1,13 @@
+import "./Banner.css";
+const Banner = () => {
+    return (
+        <div className="banner">
+            <div className="Banner-container">
+                <h1>Books Shop</h1>
+                <p>The best place to find your books</p>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
